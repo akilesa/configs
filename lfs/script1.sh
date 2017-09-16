aa3a2ff0ac39669756c45a677df0efccf5815316
akilesa <akilesa@gmail.com>
2017-09-16 17:24:09 +0300

    script 1 updated

--- lfs/script1.sh
+++ lfs/script1.sh
@@ -197,6 +197,29 @@ cd $LFS/sources
 rm -Rf linux-4.12.7
 
 
+tar -xf glibc-2.26.tar.xz
+cd glibc-2.26
+mkdir -v build
+cd       build
+../configure                             \
+      --prefix=/tools                    \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=3.2             \
+      --with-headers=/tools/include      \
+      libc_cv_forced_unwind=yes          \
+      libc_cv_c_cleanup=yes
+make
+make install
+
+echo 'int main(){}' > dummy.c
+$LFS_TGT-gcc dummy.c
+readelf -l a.out | grep ': /tools'
+rm -v dummy.c a.out
+
+cd $LFS/sources
+rm -Rf glibc-2.26
+
 
 
 
