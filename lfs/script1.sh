29bf785ce646827cd2fd045a463fc0bd5cc4df85
akilesa <akilesa@gmail.com>
2017-09-16 16:39:29 +0300

    script 1 updated

--- lfs/script1.sh
+++ lfs/script1.sh
@@ -83,3 +83,30 @@ mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 wget --input-file=packages_urls.txt --continue --directory-prefix=$LFS/sources
 
+mkdir -v $LFS/tools
+ln -sv $LFS/tools /
+
+groupadd lfs
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+passwd lfs
+chown -v lfs $LFS/tools
+chown -v lfs $LFS/sources
+su - lfs
+
+cat > ~/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+cat > ~/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+EOF
+
+source ~/.bash_profile
+
+
