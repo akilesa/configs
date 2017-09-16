1314d9e7187a0536895ff8c592e528a565db2fd9
akilesa <akilesa@gmail.com>
2017-09-16 16:46:55 +0300

    script 1 updated

--- lfs/script1.sh
+++ lfs/script1.sh
@@ -109,4 +109,25 @@ EOF
 
 source ~/.bash_profile
 
+cd $LFS/sources
+
+tar -xf binutils-2.29.tar.bz2
+cd binutils-2.29
+mkdir -v build
+cd build
+../configure --prefix=/tools            \
+             --with-sysroot=$LFS        \
+             --with-lib-path=/tools/lib \
+             --target=$LFS_TGT          \
+             --disable-nls              \
+             --disable-werror
+make
+case $(uname -m) in
+  x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
+make install
+cd $LFS/sources
+rm -Rf binutils-2.29
+
+
 
