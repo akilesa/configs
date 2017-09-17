02c08001ee6511f6ca0524a76be2af4ec1721a28
akilesa <akilesa@gmail.com>
2017-09-17 18:21:06 +0300

    packages

--- lfs/script1.sh
+++ lfs/script1.sh
@@ -371,3 +371,15 @@ make install
 cd $LFS/sources
 rm -Rf ncurses-6.0
 
+
+tar -xf bash-4.4.tar.gz
+cd bash-4.4
+./configure --prefix=/tools --without-bash-malloc
+make
+make tests
+make install
+ln -sv bash /tools/bin/sh
+cd $LFS/sources
+rm -Rf bash-4.4
+
+
